94857db077be51a73f05b9b978b054126cce6375
Aljaž Berglez <aljaz.berglez@agenda.si>
2022-01-24 06:05:19 +0100

    Inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todos/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
